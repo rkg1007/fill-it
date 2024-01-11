66eff9fe01ab11362f3e476ac8de24115ec94116
rohit <rohit.gupta@neosoftmail.com>
2024-01-11 21:56:11 +0530

    added authentication middleware

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -4,6 +4,10 @@ import * as config from './config';
 const jwtConfigs = config.get('JWT_CONFIG');
 const { secretKey } = jwtConfigs;
 
-export const createToken = async (payload) => {
+export const createToken = (payload) => {
   return jwt.sign(payload, secretKey);
+};
+
+export const verifyToken = (token) => {
+  return jwt.verify(token, secretKey);
 };
