7020584e6b38162ff7fc403f18514a2b0b0be3a5
anushayerram2025 <106475795+anushayerram2025@users.noreply.github.com>
2024-01-13 19:17:09 +0530

    Update user.js

--- src/repository/user.js
+++ src/repository/user.js
@@ -1,29 +1,31 @@
-import db from '../database'
+import * as db from '../database/mongodb';
 
-export default async function getAllUsers(){
-    const users_db=await db.collection('users');
-    const users=await users_db.find();
-    return users;
-
+export async function getAllUsers() {
+  const users_db = await db.getCollection('users');
+  const user = await users_db.insertOne({ name: 'Anusha', password: 'arjun', UserId: 'anu' });
+  const users = await users_db.find(user);
+  console.log(users);
+  return users;
 }
 
-
-export default async function getUser(userId){
-const users_db=await db.collection('users');
-const users=await users_db.find({USERId:userid});
-return users;
-
+export async function getUser(userId) {
+  const users_db = await db.getCollection('users');
+  const users = await users_db.find({ UserId: userId });
+  console.log('GOT YOU');
+  return users;
 }
 
+export async function UpdateUser(userid, fullname, email, password) {
+  const pass = password;
+  var collection = await db.getCollection('users');
+  const query = { UserId: userid };
+  console.log(collection, pass);
 
-export default async function UpdateUser(userid,fullname,email,password){
-        const pass=HashPassword(password);
-        const user=db.collection('users');
-        const query={UserId:userid}
-        const new_values={$set:{FullName:fullname,Email:email,Password:pass}}
-        user.findAndModify(query,new_values)
+  const new_values = { $set: { FullName: fullname, Email: email, Password: pass } };
+  const u = await collection.updateOne(query, new_values);
+  console.log(u);
+  return u;
 }
-
 
 
 
