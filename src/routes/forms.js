7b5b628e76bc310f0cd09a11790a098c09e25cfb
anushayerram2025 <106475795+anushayerram2025@users.noreply.github.com>
2024-02-10 10:47:04 +0530

    Update forms.js

--- src/routes/forms.js
+++ src/routes/forms.js
@@ -1,35 +1,42 @@
-import authenticate from '../auth/authenticate'
-import authorisation from '../auth/authorisation'
+import authenticate from '../auth/authenticate';
+import authorisation from '../auth/authorisation';
+import * as formService from '../controllers/forms.js';
 
 export default [
-    {
-        method:'get',
-        path:'/',
-        middlewares:[authenticate,authorisation]
-    }
-    {
-        method:'post',
-        path:'/',
-        middlewares:[authenticate,authorisation]
-    }
-    {
-        method:'get',
-        path:'/:formId',
-        middlewares:[authenticate]
-    }
-    {
-        method:'get',
-        path:'/:formId/answers',
-        middlewares:[authenticate,authorisation]
-    }
-    {
-        method:'get',
-        path:'/:formId/answers/:answerId',
-        middlewares:[authenticate,authorisation]
-    }
-    {
-        method:'get',
-        path:'/:formId/fill',
-        middlewares:[authenticate]
-    }
-]
+  {
+    method: 'get',
+    path: '/',
+    middlewares: [authenticate, authorisation],
+  },
+  {
+    method: 'post',
+    path: '/',
+    middlewares: [authenticate, authorisation, formService.createForm],
+  },
+  {
+    method: 'get',
+    path: '/:formId',
+    middlewares: [authenticate, formService.getForm],
+  },
+  {
+    method: 'post',
+    path: '/:formId',
+    middlewares: [authenticate, formService.saveResponse],
+  },
+  {
+    method: 'get',
+    path: '/:formId/responses',
+    middlewares: [authenticate, authorisation],
+  },
+  {
+    method: 'get',
+    path: '/:formId/responses/:responseId',
+    middlewares: [authenticate, authorisation],
+  },
+  {
+    method: 'get',
+    path: '/:formId/fill',
+    middlewares: [authenticate],
+  },
+];
+
