d8adc82661603b26f2ab9c3cf5bf81b878321e12
anushayerram2025 <106475795+anushayerram2025@users.noreply.github.com>
2024-02-13 19:41:15 +0530

    Update forms.js

--- src/controllers/forms.js
+++ src/controllers/forms.js
@@ -1,4 +1,10 @@
 import * as formService from './services/forms.js';
+
+async function getForms(req) {
+  const adminId = req.user;
+  const allForms = await formService.getForms(adminId);
+  return allForms;
+}
 async function getForm(req) {
   const formId = req.params.formId;
 
@@ -9,16 +15,25 @@ async function getForm(req) {
 
 async function saveResponse(req) {
   const formId = req.params.formId;
-  return await formService.saveResponse(formId, req.body);
+  return await formService.saveResponse(formId, req.body, req.user);
 }
 
 async function createForm(req) {
-  return await formService.saveResponse(req.body);
+  return await formService.createForm(req.body, req.user);
 }
 
 async function editForm(req) {
   const formId = req.params.formId;
   await formService.editForm(formId, req.user);
 }
+async function getResponses(req) {
+  const formId = req.body.formId;
 
-export default [getForm, saveResponse, createForm, editForm];
+  return await formService.getResponses(formId);
+}
+async function getResponse(req) {
+  const { formId, id: responseId } = req.body;
+  return await formService.getResponse(formId, responseId);
+}
+
+export default [getForms, getForm, saveResponse, createForm, editForm, getResponse, getResponses];
