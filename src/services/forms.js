ae0c1c3104f8e9068ba0798ad7471f94b2e4c62f
anushayerram2025 <106475795+anushayerram2025@users.noreply.github.com>
2024-02-13 19:39:26 +0530

    Update forms.js

--- src/services/forms.js
+++ src/services/forms.js
@@ -1,5 +1,10 @@
 import getJoiSchema from '../helpers/joi validators';
-import * as formService from './repository/forms.js';
+import * as formService from './repositories/forms.js';
+
+async function getForms(admin) {
+  const allForms = await formService.getForms(admin);
+  return allForms;
+}
 
 async function getForm(formId) {
   return await formService.getForm(formId);
@@ -17,11 +22,18 @@ async function saveResponse(formId, responses) {
 }
 
 async function createForm(formDetails) {
-  return formService.createForm(formDetails);
+  return await formService.createForm(formDetails);
 }
 
 async function editForm(formId, user) {
-  return formService.editForm(formId, user);
+  return await formService.editForm(formId, user);
 }
 
-export default [getForm, saveResponse, createForm, editForm];
+async function getResponses(formId) {
+  return await formService.getResponses(formId);
+}
+async function getResponse(formId, responseId) {
+  return await formService.getResponse(formId, responseId);
+}
+
+export default [getForms, getForm, saveResponse, createForm, editForm, getResponses, getResponse];
