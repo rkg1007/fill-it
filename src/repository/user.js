d74b01a7aefeb25578dfc81b21ada1766f4117b8
anushayerram2025 <106475795+anushayerram2025@users.noreply.github.com>
2024-02-13 19:38:27 +0530

    Update user.js

--- src/repository/user.js
+++ src/repository/user.js
@@ -1,21 +1,23 @@
-import * as db from '../database/mongodb';
+import { getCollection } from '../database/mongodb';
 
 export async function getAllUsers() {
-  const users_db = await db.getCollection('users');
+  const users_db = await getCollection('users');
+  const user = await users_db.insertOne({ name: 'Anusha', password: 'arjun', UserId: 'anu' });
   const users = await users_db.find(user);
   console.log(users);
   return users;
 }
 
 export async function getUser(userId) {
-  const users_db = await db.getCollection('users');
+  const users_db = await getCollection('users');
   const users = await users_db.find({ UserId: userId });
+  console.log('GOT YOU');
   return users;
 }
 
 export async function UpdateUser(userid, fullname, email, password) {
   const pass = password;
-  var collection = await db.getCollection('users');
+  var collection = await getCollection('users');
   const query = { UserId: userid };
   console.log(collection, pass);
 
@@ -24,6 +26,17 @@ export async function UpdateUser(userid, fullname, email, password) {
   console.log(u);
   return u;
 }
+
+export const getUserByEmail = async (email) => {
+  const userCollection = await getCollection('users');
+  return userCollection.findOne({ email });
+};
+
+export const createUser = async (user) => {
+  const userCollection = await getCollection('users');
+  const result = await userCollection.insertOne(user);
+  return result.insertedId;
+};
 
 
 
