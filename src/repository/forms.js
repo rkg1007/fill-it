0f0711897e1fade6ba965464a342ec7fac8a7b5e
anushayerram2025 <106475795+anushayerram2025@users.noreply.github.com>
2024-02-13 19:35:41 +0530

    Update forms.js

--- src/repository/forms.js
+++ src/repository/forms.js
@@ -3,23 +3,47 @@ import { getCollection } from '../database/mongodb';
 var forms = getCollection('forms');
 var responses = getCollection('responses');
 
+export async function getForms(admin) {
+  const query = { CreatedBy: admin };
+  const formDetails = await forms.find(query);
+  return formDetails;
+}
+
+export async function getResponses(formId) {
+  const query = { FormId: formId };
+  const responses = await forms.find(query);
+  return responses;
+}
+export async function getResponse(formId, responseId) {
+  const query = { FormId: formId, id: responseId };
+  const response = await forms.find(query);
+  return response;
+}
 export async function getForm(formId) {
   const query = { FormId: formId };
   const formDetails = await forms.find(query);
   return formDetails;
 }
 
-export async function saveResponse(formId, response) {
-  //already got validated....(either using middleware or in service file)
-
-  (response.FormId = formId), await responses.insertOne(response);
+export async function saveResponse(formId, response, userId) {
+  let record = { UserId: userId, Response: response, latestUpdateAt: new Date(), FormId: formId };
+  await responses.insertOne(record);
 }
 
-export async function createForm(formDetails) {
-  await forms.insertOne(formDetails);
+export async function createForm(fieldDetails, admin) {
+  let record = { CreatedBy: admin, Fields: fieldDetails, latestUpdateAt: new Date() };
+  await responses.insertOne(record);
 }
 
 export async function editForm(formId, userId) {
   const response = await responses.findOne({ FormId: formId, UserId: userId });
   return response;
+}
+export async function check(responseId, userId) {
+  const record = await responses.findOne({ id_: responseId });
+  if (record.createdBy == userId || record.UserId == userId) {
+    return true;
+  } else {
+    return false;
+  }
 }
