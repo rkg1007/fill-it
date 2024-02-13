8201ca579b1c330a570c45bec5dd0b8224e9fccf
anushayerram2025 <106475795+anushayerram2025@users.noreply.github.com>
2024-02-13 19:49:16 +0530

    Update user.js

--- src/services/user.js
+++ src/services/user.js
@@ -1,17 +1,17 @@
-import * as userRepo from '../repository/user.js';
+import * as userService from '../repository/user.js';
 
 export const getAllUsers = async () => {
-  return await userRepo.getAllUsers();
+  return await userService.getAllUsers();
 };
 
 export var getUser = async (user) => {
   const userId = user.userid;
-  return await userRepo.getUser(userId);
+  return await userService.getUser(userId);
 };
 
 export async function UpdateUser(req) {
   const { userid, fullName, email, password } = req.body;
-  const user = await userRepo.UpdateUser(userid, fullName, email, password);
+  const user = await userService.UpdateUser(userid, fullName, email, password);
   return user;
 }
 
