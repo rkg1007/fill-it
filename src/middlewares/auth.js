555ff82cfe3e656602307c0820383c46f29b7d76
rohit <rkg10072000@gmail.com>
2024-03-03 23:05:23 +0530

    fixed bugs

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,27 +1,27 @@
 import UnauthenticatedError from '../errors';
 import * as jwt from '../utils/jwt';
 
-export default async function () {
-  return async function (req, _res, next) {
-    const user = await getLoggedInUser(req);
+export default function () {
+  return (req, _res, next) => {
+    const user = getLoggedInUser(req);
 
     if (!user) {
       throw new UnauthenticatedError('User is not logged in.');
     }
 
-    req.user = user;
+    req.loggedInUser = user;
     next();
   };
 }
 
-const getLoggedInUser = async (req) => {
+const getLoggedInUser = (req) => {
   const authorizationHeader = req.get('authorization');
   if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
     const token = authorizationHeader.split(' ')[1];
     try {
       const payload = jwt.verifyToken(token);
       return payload;
-    } catch (error) {
+    } catch {
       return null;
     }
   }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,10 @@
+import authCheck from '../middlewares/auth';
 import homeRoutes from './home';
 import authRoutes from './auth';
+import userRoutes from './users';
 
 export default [
   { prefix: '', routes: homeRoutes },
   { prefix: '/auth', routes: authRoutes },
-  { prefix: '/user', routes: userRoutes },
+  { prefix: '/users', routes: userRoutes, middlewares: [authCheck()] },
 ];
